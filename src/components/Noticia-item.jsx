5cc64f2f11ef578858a9f35471ff686a1d18dfb9
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-09 15:13:01 -0500

    Se agrega Componente de Modal

--- src/components/Noticia-item.jsx
+++ src/components/Noticia-item.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import '../css/NoticiaItem.css'
 import User from './User'
 
-const replaceText = (text) => {
-  let textModifer
-  textModifer = text.replace("<p>", "")
-  textModifer = textModifer.replace("</p>", "")
-  textModifer = textModifer.replace("<b>", "")
-  textModifer = textModifer.replace("</b>", "")
-  textModifer = textModifer.replace("<i>", "")
-  textModifer = textModifer.replace("</i>", "")
-  return textModifer
-}
-
 const NoticiaItem = props => (
   <div className="NoticiaItem">
-    <User image={ props.album.images[0].url } name={props.artists[0].name} />
-    <img className="NoticiaItem-img" src={ props.album.images[0].url } alt={props.album.name}/>
+    
+    <User image={ props.album.images[1].url ? props.album.images[1].url : props.album.images[0].url } 
+          name={props.artists[0].name} 
+          />
+
+    <img className="NoticiaItem-img" 
+         src={ props.album.images[0].url } 
+         alt={props.album.name}
+         />
+
     <p className="NoticiasItem-name"> {props.album.name} </p>
+
   </div>
 )
 
---
+++ src/components/modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './modal.sass'
+
+function Modal (props) {
+  return (
+    <div className="Modal">
+      { props.children }
+      {
+        props.botonVisible && (
+          <button className="Modal-close" onClick={props.handleClick} >&times;</button>
+        )
+      }
+    </div>
+  )
+}
+
+export default Modal
