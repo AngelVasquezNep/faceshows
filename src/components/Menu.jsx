2725b16f3b70e73cf8ae7c682774985814c86b04
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-10 17:45:22 -0500

    Se logra que las notificaciones tengan contenido interactivo sin cerrarce mientras se cambia de una a otra

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../css/app.css'
 import '../css/Menu.css'
 import Search from './Search-Container'
 // import logo from '../images/facebook.png'
 import tv from '../images/tv.png'
-import users from '../images/friends.png'
+import friends from '../images/friends.png'
 import message from '../images/message.png'
 import world from '../images/world.png'
 import help from '../images/help.png'
@@ -14,52 +14,110 @@ import NotificationMessage from './NotificationMessage'
 
 const Logo = "/images/tickets.png"
 
-const Menu = props => (
-  <div className="Menu-Container">
-    <div className="Container Menu">
-      <div className="Menu--buscador">
-        <img className="Menu--logo"
-          src={Logo} alt="Logo"/>
-        
-        <Search
-          handleSearch = {props.handleSearch}
-        />
+class Menu extends Component {
+
+  state = {
+    Artistas: [],
+    messages: [],
+    noticias: [],
+    typeNotification: '',
+    isShowNotification: false
+    
+  }
+
+  handleClickMenu = ev => {
+    
+    console.log(this.menuItem === ev.target.id)
+    
+    this.setState({
+      typeNotification: ev.target.id,
+      isShowNotification: true
+    })
+    
+    if (this.menuItem === ev.target.id && this.state.isShowNotification) {
+      this.setState({isShowNotification: false})
+      console.log("Cerrar")
+    }
+    this.menuItem = ev.target.id
+    
+  }
+
+  render() {
+
+    return (
+
+      <div className="Menu-Container">
+        <div className="Container Menu">
+          
+          <div className="Menu--buscador">
+            <img className="Menu--logo"
+            src={Logo} alt="Logo"/>
       
+            <Search
+              handleSearch = {this.props.handleSearch}
+            />
+          
+          </div>
+          
+          <nav className="Dashboard">
+            
+            <a className="Usuario" href="/">
+              <img src={tv} alt="TV" className="Usuario--img"/>
+              <span className="Usuario-name">Angel</span>
+            </a>
+            
+            <a className="Inicio" href="/">
+              <span>Inicio</span>
+            </a>
+
+            <div  className="Users" 
+                  title="Solicitudes de Programas" 
+                  onClick = { this.handleClickMenu } 
+                  >
+                  <img src={friends} alt="Friends" id="Artistas"/>
+            </div>
+
+            <div  className="Message" 
+                  title="Mensajes" 
+                  onClick = { this.handleClickMenu } 
+                  >
+                  <img src={message} alt="Message" id="Messages"/>
+            </div>
+
+            <div  className="Menu-Noticias"
+                  title="Noticias"
+                  onClick = { this.handleClickMenu } 
+                  >
+              <img src={world} alt="News" id="Noticias"/>
+            </div>
+
+            <div className="Help" title="Ayuda Rápida">
+              <img src={help} alt="Help"/>
+            </div>
+
+            <div className="Config" title="Configuración">
+              <img src={triangle} alt="Configuración"/>
+            </div>
+
+          
+          </nav>
+          {
+            this.state.isShowNotification && 
+            <ContainerNotification>
+              
+              <NotificationMessage
+                typeNotification = {this.state.typeNotification}
+              />
+            
+            </ContainerNotification>
+          }
+            
+        
+        </div>
       </div>
-      <nav className="Dashboard">
-        <a className="Usuario" href="/">
-          <img src={tv} alt="TV" className="Usuario--img"/>
-          <span className="Usuario-name">Angel</span>
-        </a>
-        <a className="Inicio" href="/">
-          <span>Inicio</span>
-        </a>
-        <div className="Users" title="Solicitudes de Programas" >
-            <img src={users} alt="Users"/>
-        </div>
-        <div className="Message" title="Mensajes" >
-            <img src={message} alt="Message"/>
-        </div>
-        <div className="Menu-Noticias" title="Noticias" >
-            <img src={world} alt="News"/>
-        </div>
-        <div className="Help" title="Ayuda Rápida">
-            <img src={help} alt="Help"/>
-        </div>
-        <div className="Config" title="Configuración">
-            <img src={triangle} alt="Configuración"/>
-        </div>
-      </nav>
+    )
+  }
+}
 
-      <ContainerNotification>
-        <NotificationMessage
-          typeNotification = "Noticias"
-        />
-      </ContainerNotification>
-
-
-    </div>
-  </div>
-)
 
 export default Menu
--- src/components/NotificationMessage.jsx
+++ src/components/NotificationMessage.jsx
@@ -4,6 +4,7 @@ const NotificationMessage = props => (
   <div className="NotificationMessage">
     <h5 className="NotificationMessage-type"> { props.typeNotification } </h5 >
     <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem corrupti cupiditate sapiente atque delectus cumque consectetur quo numquam modi quae est, facilis ad soluta repellendus recusandae neque ut dignissimos dolores? </p>
+    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem corrupti cupiditate sapiente atque delectus cumque consectetur quo numquam modi quae est, facilis ad soluta repellendus recusandae neque ut dignissimos dolores? </p>
   </div>
 )
 
