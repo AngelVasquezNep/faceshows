6e033c530fa180c42856bbc5f3fccb3b20b3ff9b
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-10 02:58:52 -0500

    Se agregan estilos al modal

--- src/components/ItemFullScreen.jsx
+++ src/components/ItemFullScreen.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import '../css/ItemFullScreen.css'
 
 const ItemFullScreen = props => (
-  <div className="ItemFullScreen">
-    <img className="NoticiaItem-img" 
-         src={ props.item.album.images[0].url } 
-         alt={props.item.album.name}
-         />
+  <div className="ItemFullScreen" 
+  >
+    <div className="ItemFullScreen-background"
+      style={{ backgroundImage: 'url(' + props.item.album.images[0].url + ')' }} 
+    >
+    </div>
+
+    <div className="ItemFullScreen-content">
+    
+      <img className="ItemFullScreen-img" 
+          src={ props.item.album.images[0].url } 
+          alt={props.item.album.name}
+          />
+
+
+      <div className="ItemFullScreen-info">
+        <h1>{props.item.album.name}</h1>
+        <h2 className="ItemFullScreen-title"> {props.item.artists[0].name} </h2>
+      </div>
+    
+    </div>
+
   </div>
 )
 
