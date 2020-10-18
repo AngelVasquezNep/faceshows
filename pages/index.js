beb90e3bf7ab7657524b311dd483e06f6972e32b
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2020-10-18 14:32:59 -0500

    Fisrt page structure and Construction message

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+
+const Index = () => (
+  <Fragment>
+    <Head>
+      <title>🔥 Faceshows</title>
+    </Head>
+
+    <h1>Wellcome</h1>
+
+    <p>We are on refactor 🔥🔥🔥🔥...🚀</p>
+  </Fragment>
+)
+
+export default Index
